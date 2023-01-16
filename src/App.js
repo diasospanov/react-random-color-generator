d8b54ba9ef5b9233c77aab6cbded033852a0a91c
Di <dias.ospanov@gmail.com>
2023-01-16 15:00:11 +0200

    Create first draft.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Random Color Generator</h1>
+      <br />
+      <button onClick={() => {}}>Generate</button>
+      <br />
+      <br />
+      <input onChange={(event) => {}}></input>
+      <br />
+      <br />
+      <div
+        style={{
+          backgroundColor: 'blue',
+          width: '155px',
+          fontSize: '20px',
+        }}
+      >
+        Generated Color:
+      </div>
+    </div>
+  );
+}
